5f6b1f57f213712658fb4c6c862657fb2ef72979
Badru9 <mohbadrujaman9@gmail.com>
2026-07-02 18:33:01 +0700

    fix: change global_position to local position before adding nodes to SceneTree

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,13 +11,13 @@ func _ready() -> void:
 	var player1 = preload("res://scenes/player.tscn").instantiate()
 	player1.player_id = 1
 	player1.name = "Player1"
-	player1.global_position = Vector3(-2, 1, 0)
+	player1.position = Vector3(-2, 1, 0)
 	viewport1.get_node("Map").add_child(player1)
 	
 	# Instantiate player 2 di Viewport 2 (berbagi world_3d yang sama)
 	var player2 = preload("res://scenes/player.tscn").instantiate()
 	player2.player_id = 2
 	player2.name = "Player2"
-	player2.global_position = Vector3(2, 1, 0)
+	player2.position = Vector3(2, 1, 0)
 	viewport2.add_child(player2)
 

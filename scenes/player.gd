e45a04efe9cf6cdc2ffd95a7630a83c2a498be44
Badru9 <mohbadrujaman9@gmail.com>
2026-07-02 18:44:17 +0700

    feat: implement mouse look, multiplayer authority, and property replication

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,49 +1,58 @@
 extends CharacterBody3D
 
-@export var player_id: int = 1
 @export var speed: float = 5.0
 @export var jump_velocity: float = 4.5
+@export var mouse_sensitivity: float = 0.003
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera: Camera3D = $CameraPivot/Camera3D
+
+func _enter_tree() -> void:
+	# Jadikan nama node sesuai dengan peer id multiplayer agar mudah disinkronkan
+	set_multiplayer_authority(name.to_int())
+
+func _ready() -> void:
+	if is_multiplayer_authority():
+		camera.make_current()
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		camera.visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority():
+		return
+		
+	# Mouse look
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		camera_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Tambahkan gravitasi
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Dapatkan string input aksi yang dinamis berdasarkan player_id
-	var move_forward = "p%d_move_forward" % player_id
-	var move_back = "p%d_move_back" % player_id
-	var move_left = "p%d_move_left" % player_id
-	var move_right = "p%d_move_right" % player_id
+	# Hanya multiplayer authority yang memproses input lokal
+	if not is_multiplayer_authority():
+		return
 
-	# Baca input vector
-	var input_dir := Vector2.ZERO
-	if Input.is_action_pressed(move_left):
-		input_dir.x -= 1.0
-	if Input.is_action_pressed(move_right):
-		input_dir.x += 1.0
-	if Input.is_action_pressed(move_forward):
-		input_dir.y -= 1.0
-	if Input.is_action_pressed(move_back):
-		input_dir.y += 1.0
+	# Handle Jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
 
-	# Rotasi horizontal sederhana dengan keyboard
-	var rotation_dir := 0.0
-	if player_id == 1:
-		if Input.is_key_pressed(KEY_Q):
-			rotation_dir += 1.0
-		if Input.is_key_pressed(KEY_R):
-			rotation_dir -= 1.0
-	elif player_id == 2:
-		if Input.is_key_pressed(KEY_Y):
-			rotation_dir += 1.0
-		if Input.is_key_pressed(KEY_P):
-			rotation_dir -= 1.0
+	# Baca input pergerakan
+	var input_dir = Input.get_vector("p1_move_left", "p1_move_right", "p1_move_forward", "p1_move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	rotate_y(rotation_dir * 2.5 * delta)
-
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
